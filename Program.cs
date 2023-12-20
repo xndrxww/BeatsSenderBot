8b18844072e2a8ba928e6f3bf237a787cb6ce32e
Андрей Кузнецов <blacksilver@ro.ru>
2023-12-20 20:28:13 +0300

    Регистрация бота, добавление обработчика для сообщений, вывод меню

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace BeatsSenderBot
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+
+namespace BeatsSenderBot
 {
-    internal class Program
+    public class Program
     {
+        private static readonly ITelegramBotClient _botClient = new TelegramBotClient("6367612655:AAH8VBioHZrYKkNPOoObXQ6ZsVgzFo9PQ8U");
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StartInfo();
+
+            var handler = new UpdateHandler();
+            var cancellationToken = new CancellationToken();
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { } //принимает все изменения
+            };
+
+            _botClient.StartReceiving(handler, receiverOptions, cancellationToken);
+            Console.ReadLine();
+        }
+
+        private static void StartInfo()
+        {
+            var botInfo = _botClient.GetMeAsync().Result;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Запуск бота: {botInfo.FirstName}");
+            Console.WriteLine($"Информация: {botInfo}");
+            Console.WriteLine();
+            Console.ResetColor();
         }
     }
 }
---
+++ UpdateHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace BeatsSenderBot
+{
+    public class UpdateHandler : IUpdateHandler
+    {
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == UpdateType.Message)
+            {
+                var message = update.Message;
+                var userName = message.Chat.Username;
+
+
+                if (message.Text.ToLower() == "/start")
+                {
+                    var inlineKeyBoard = new InlineKeyboardMarkup(
+                        new[]
+                        {
+                            new[]
+                            {
+                                InlineKeyboardButton.WithCallbackData(text: "Почты", callbackData: "email"),
+                            },
+                            new[]
+                            {
+                                InlineKeyboardButton.WithCallbackData(text: "Авторизация", callbackData: "authorization"),
+                            },
+                            new[]
+                            {
+                                InlineKeyboardButton.WithCallbackData(text: "Рассылка", callbackData: "mailing")
+                            }
+                        });
+
+                    await botClient.SendTextMessageAsync(message.Chat.Id, $"Пользователь {userName} запустил бота!", replyMarkup: inlineKeyBoard);
+
+                    Console.WriteLine($"Сообщение отправлено. Username пользователя: {userName}");
+                }
+            }
+        }
+
+        public async Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Произошла ошибка: {JsonSerializer.Serialize(exception)}");
+        }
+    }
+}
