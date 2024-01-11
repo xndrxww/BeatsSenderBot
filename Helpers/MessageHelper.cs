af296a21930c70ab9bdd5f0fdb3f336af4c573c7
Андрей Кузнецов <blacksilver@ro.ru>
2024-01-11 23:42:41 +0300

    Рефакторинг, правки

---
+++ Helpers/MessageHelper.cs
@@ -0,0 +1,59 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace BeatsSenderBot.Helpers
+{
+    public static class MessageHelper
+    {
+        public static async void SendStartMessage(ITelegramBotClient botClient, Message message)
+        {
+            var userName = message.Chat.Username;
+
+            var inlineKeyBoard = new InlineKeyboardMarkup(
+                new[]
+                {
+                    new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "Почты", callbackData: "email"),
+                    },
+                    new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "Авторизация", callbackData: "authorization"),
+                    },
+                    new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "Рассылка", callbackData: "mailing")
+                    }
+                });
+
+            await botClient.SendTextMessageAsync(message.Chat.Id, $"Пользователь {userName} запустил бота!", replyMarkup: inlineKeyBoard);
+        }
+
+        public static async void SendDocument(ITelegramBotClient botClient, Message message)
+        {
+            var file = await botClient.GetFileAsync(message.Document.FileId);
+
+            var folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files");
+            var fileName = "BeatSender.txt";
+            var filePath = Path.Combine(folderPath, fileName);
+
+            try
+            {
+                using (var fs = new FileStream(filePath, FileMode.Create))
+                {
+                    await botClient.DownloadFileAsync(file.FilePath, fs);
+                }
+
+                //EmailHelper.SendEmail(filePath);
+
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Файл отправлен");
+            }
+            catch (Exception e)
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, "При загрузке/отправке файла произошла ошибка");
+                Console.WriteLine($"Ошибка при загрузке файла {e}");
+            }
+        }
+    }
+}
--- UpdateHandler.cs
+++ UpdateHandler.cs
@@ -4,7 +4,6 @@ using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.ReplyMarkups;
 
 namespace BeatsSenderBot
 {
@@ -15,49 +14,14 @@ namespace BeatsSenderBot
             if (update.Type == UpdateType.Message)
             {
                 var message = update.Message;
-                var userName = message.Chat.Username;
 
                 if (message.Text?.ToLower() == "/start")
                 {
-                    var inlineKeyBoard = new InlineKeyboardMarkup(
-                        new[]
-                        {
-                            new[]
-                            {
-                                InlineKeyboardButton.WithCallbackData(text: "Почты", callbackData: "email"),
-                            },
-                            new[]
-                            {
-                                InlineKeyboardButton.WithCallbackData(text: "Авторизация", callbackData: "authorization"),
-                            },
-                            new[]
-                            {
-                                InlineKeyboardButton.WithCallbackData(text: "Рассылка", callbackData: "mailing")
-                            }
-                        });
-
-                    await botClient.SendTextMessageAsync(message.Chat.Id, $"Пользователь {userName} запустил бота!", replyMarkup: inlineKeyBoard, cancellationToken: cancellationToken);
-
-                    Console.WriteLine($"Сообщение отправлено. Username пользователя: {userName}");
+                    MessageHelper.SendStartMessage(botClient, message);
                 }
-
                 if (message.Type == MessageType.Document)
                 {
-                    var path = @"D:\C# Projects\BeatsSenderBot\Files\"; //TODO изменить
-
-                    var file = await botClient.GetFileAsync(message.Document.FileId, cancellationToken);
-                    var fileName = "BeatSender.txt";
-
-                    var filePath = Path.Combine(path, fileName);
-
-                    using (var fs = new FileStream(filePath, FileMode.Create))
-                    {
-                        await botClient.DownloadFileAsync(file.FilePath, fs);
-                    }
-
-                    EmailHelper.SendEmail(filePath);
-
-                    await botClient.SendTextMessageAsync(message.Chat.Id, "Файл отправлен", cancellationToken: cancellationToken);
+                    MessageHelper.SendDocument(botClient, message);
                 }
             }
             if (update.Type == UpdateType.CallbackQuery)
@@ -66,9 +30,7 @@ namespace BeatsSenderBot
 
                 if (buttonCode == "mailing")
                 {
-                    var chatId = update.CallbackQuery.Message.Chat.Id;
-
-                    await botClient.SendTextMessageAsync(chatId, "Прикрепите файл для рассылки", cancellationToken: cancellationToken);
+                    CallbackQueryHelper.GetMailing(botClient, update);
                 }
             }
         }
